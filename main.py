37cf372f8c34a5d11be25a82d37833c61567fb22
codewarnab <dp950810@gmail.com>
2025-02-19 02:09:23 +0530

    some chanages

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 app = FastAPI()
 
+@app.get("/")
+async def home_root():
+    return {"message": "Hello World"}
+
+
 @app.get("/transcript/{video_id}")
 async def get_transcript(video_id: str):
     try:
